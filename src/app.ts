5cf91685db37090f8b910511090b054e6f524cef
Hugo Deiró <hugodeiro@gmail.com>
2022-10-25 15:38:16 -0300

    Update the way logs were booted

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 import * as cors from 'cors';
-import helmet from 'helmet';
 import * as bodyParser from 'body-parser';
-import { configure } from "log4js";
+import helmet from 'helmet';
 import routeDefs from "./endpoints/engine/route";
+import startLogEngine from './utils/log-configurations';
+import { rootDir } from './utils/path';
 
-const path    = require('path');
-const rootDir = require('./utils/path');
+const path = require('path');
 
 // Load Environment Variables
 dotenv.config();
+
+// Start log configs
+startLogEngine();
 
 // Create Express Instance of the App
 const app = express();
@@ -21,51 +24,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(rootDir, 'assets')));
 
-// Defining Logs
-configure({
-  appenders: {
-    access: {
-      type: "dateFile",
-      filename: `${process.env.LOG_PATH}/base-access.log`,
-      pattern: "-yyyy-MM-dd",
-      category: "http"
-    },
-    app: {
-      type: "file",
-      filename: `${process.env.LOG_PATH}/base-api.log`,
-      maxLogSize: 10485760,
-      numBackups: 3
-    },
-    errorFile: {
-      type: "file",
-      filename: `${process.env.LOG_PATH}/base-api-errors.log`
-    },
-    errors: {
-      type: "logLevelFilter",
-      level: "ERROR",
-      appender: "errorFile"
-    }
-  },
-  categories: {
-    default: {
-      appenders: [ "app", "errors" ],
-      level: process.env.LOG_LEVEL
-    },
-    http: {
-      appenders: [ "access"],
-      level: process.env.LOG_LEVEL
-    }
-  }
-});
-
 // Routes
 app.use(routeDefs.path, routeDefs.router);
 app.get('/', (_, res) => res.send('Node App!'));
 
 // Listener
-const server = app.listen(process.env.PORT, () => {
-  return console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`);
-});
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Listening on ${process.env.HOST}:${process.env.PORT}`));
 
 // Exports
-module.exports = { app, server }
+module.exports = { app, server };
---
+++ src/utils/log-configurations.ts
@@ -0,0 +1,39 @@
+import { configure } from "log4js";
+
+export default function startLogEngine() {
+  configure({
+    appenders: {
+      access: {
+        type: "dateFile",
+        filename: `${process.env.LOG_PATH}/base-access.log`,
+        pattern: "-yyyy-MM-dd",
+        category: "http"
+      },
+      app: {
+        type: "file",
+        filename: `${process.env.LOG_PATH}/base-api.log`,
+        maxLogSize: 10485760,
+        numBackups: 3
+      },
+      errorFile: {
+        type: "file",
+        filename: `${process.env.LOG_PATH}/base-api-errors.log`
+      },
+      errors: {
+        type: "logLevelFilter",
+        level: "ERROR",
+        appender: "errorFile"
+      }
+    },
+    categories: {
+      default: {
+        appenders: [ "app", "errors" ],
+        level: process.env.LOG_LEVEL
+      },
+      http: {
+        appenders: [ "access"],
+        level: process.env.LOG_LEVEL
+      }
+    }
+  });
+}
